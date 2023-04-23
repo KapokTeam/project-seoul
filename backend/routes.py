3d2599fd640569c5282dec2c551bd98986a426b1
seojaeoh <seojaeohcoder@gmail.com>
2023-04-23 23:01:32 +0900

    patch_notes_create

--- backend/routes.py
+++ backend/routes.py
@@ -5,6 +5,10 @@ from flask import render_template
 def index():
     return render_template('home.html')
 
+@application.route('/patch')
+def patch():
+    return render_template('patch_notes.html')
+
 @application.errorhandler(404)
 def page_not_found(error):
     return render_template('error.html', error=error), 404

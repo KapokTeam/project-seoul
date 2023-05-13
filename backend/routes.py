cd1433babcdddc79b7184cbb46651f79ebb9d6a0
seojaeoh <seojaeohcoder@gmail.com>
2023-05-14 04:39:59 +0900

    list_create

--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,134 @@
 from backend import application
-from flask import request, render_template, redirect, url_for, jsonify
-from backend.views import print_geo_value, get_api_data, add_member, validate_member, validate_ID, validate_PW
+from flask import request, render_template, redirect, url_for, jsonify, session
+from backend.views import print_geo_value, get_api_total, add_member, validate_member, validate_ID, validate_PW, account_Check, create_api_data, update_api_data
+from firebase_admin import db
 
 @application.route('/', methods=['GET'])
 def index():
     return render_template('home.html')
+    # if 'nickname' in session:
+    #     return session['nickname']
+    # else:
+    #     return render_template('home.html')
 
-@application.route('/list', methods=['GET'])
+@application.route('/list', methods=['GET', 'POST'])
 def list():
-    return render_template('list.html')
+    if request.method == 'GET' or request.method == 'POST':
+        page = request.args.get('page')
+        firstpage = False
+
+        if page is None:
+            page = 1
+            firstpage = True
+        else:
+            page = int(page)
+
+        page = int(page)
+
+        search = request.args.get('searchquery') 
+        if search is None:
+            search = None
+
+        total = int(get_api_total(search))
+        x = 0
+        start = 0
+        end = 0
+
+        if firstpage is True:
+            if(page < 1):
+                page = 1
+            elif(page>total):
+                page = total
+
+            x = (page//10) # 10 // 10 = 1
+
+            if(page%10==0):
+                start = ((x-1)*10)+1
+                end = ((x)*10)+1
+            else:
+                start = (x*10)+1
+                end = ((x+1)*10)+1
+
+            if(end > total):
+                end = total+1
+
+            return redirect(url_for('list', page=1, searchquery=search))
+        elif page > total:
+            if(page < 1):
+                page = 1
+            elif(page>total):
+                page = total
+
+            x = (page//10) # 10 // 10 = 1
+
+            if(page%10==0):
+                start = ((x-1)*10)+1
+                end = ((x)*10)+1
+            else:
+                start = (x*10)+1
+                end = ((x+1)*10)+1
+
+            if(end > total):
+                end = total+1
+
+            return redirect(url_for('list', page=total, searchquery=search))
+        elif page < 1:
+            if(page < 1):
+                page = 1
+            elif(page>total):
+                page = total
+
+            x = (page//10) # 10 // 10 = 1
+
+            if(page%10==0):
+                start = ((x-1)*10)+1
+                end = ((x)*10)+1
+            else:
+                start = (x*10)+1
+                end = ((x+1)*10)+1
+
+            if(end > total):
+                end = total+1
+
+            return redirect(url_for('list', page=1, searchquery=search))
+
+        if(page < 1):
+            page = 1
+        elif(page>total):
+            page = total
+        x = (page//10) # 10 // 10 = 1
+        if(page%10==0):
+            start = ((x-1)*10)+1
+            end = ((x)*10)+1
+        else:
+            start = (x*10)+1
+            end = ((x+1)*10)+1
+        if(end > total):
+            end = total+1
+
+        ref = db.reference('piece')
+        query = ref.order_by_child('year')
+
+        if search:
+            matching_pieces = []
+            for item in query.get().values():
+                if search in item['name']:
+                    matching_pieces.append(item)
+            matching_pieces.reverse()
+            start2 = (page-1)*10
+            end2 = page*10
+            sliced_data = matching_pieces[start2:end2]
+        else:
+            data = []
+            for item in query.get().values():
+                data.append(item)
+            data.reverse() 
+            # 데이터 슬라이싱
+            start2 = (page-1)*10
+            end2 = page*10
+            sliced_data = data[start2:end2]
+
+        return render_template('list.html', start=start, end=end, current_page=page, total=total, data=sliced_data, searchquery=search)
 
 @application.route('/patch', methods=['GET'])
 def patch():
@@ -21,6 +141,11 @@ def signup():
 @application.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@application.route('/logout', methods=['GET'])
+def logout():
+    session.pop('nickname', None)
+    return redirect('/')
 
 #멤버 회원가입 조회 => 삽입
 @application.route('/member', methods=['POST'])
@@ -39,7 +164,7 @@ def member():
         else:
             return redirect(url_for('signup', alertdata = True))
     else: 
-        return redirect('index')
+        return redirect('/')
 
 
 @application.route('/checkid', methods=['POST'])
@@ -56,9 +181,38 @@ def check_pw():
         pw = data.get('pw')
         return str(validate_PW(pw))
 
-@application.route('/getdata')
-def getdata():
-    return get_api_data()
+@application.route('/accountcheck', methods=['POST'])
+def accountcheck():
+    id = request.form['id']
+    pw = request.form['password']
+    if (account_Check(id, pw)):
+        return redirect('/')
+    else:
+        return redirect(url_for('login', alertdata = True))
+
+# @application.route('/getdata', methods=['GET'])
+# def getdata():
+#     cur_page = request.args.get('page')
+
+#     if cur_page:
+#         try:
+#             cur_page = int(cur_page)
+#         except ValueError:
+#             cur_page = 1
+#     else:
+#         cur_page = 1
+    
+#     return get_api_piece(int(cur_page))
+
+@application.route('/createpiece')
+def createpiece():
+    return create_api_data()
+
+@application.route('/updatepiece')
+def updatepiece():
+    return update_api_data()
+
+
 
 @application.route('/around')
 def around():
--- backend/views.py
+++ backend/views.py
@@ -1,29 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from geopy.geocoders import Nominatim
 import requests
 import json
 from config import firebase_admin
 from firebase_admin import db
 import random
-
-def get_geo_value(address):
-    geolocoder = Nominatim(user_agent = 'South Korea', timeout=None)
-    geo = geolocoder.geocode(address)
-    crd = {"lat": str(geo.latitude), "lng": str(geo.longitude)}
-
-    return crd
-
-def print_geo_value():
-    crd = get_geo_value("서울특별시 마포구 상암동 478-1")
-    return str(crd['lat']) + " & " + str(crd['lng'])
-
-def get_api_data():
-    url = 'http://openapi.seoul.go.kr:8088/6e467277426a61633339737752416b/xml/tvGonggongAllArt/1/5/'
-
-    response = requests.get(url)
-    print(response.text)
-
-    return "json?"
+from bs4 import BeautifulSoup
+import xmltodict
+import math
 
 def add_member(id, pw, email, nickname):
     a = ["성실한", "진지한"]
@@ -36,7 +20,7 @@ def add_member(id, pw, email, nickname):
         nickname = str(random.choice(a) + " " + random.choice(b))
 
     if(trim_email == None or trim_email == ''):
-        email = False
+        email = 'none'
 
 
     ref = db.reference('member') # 기본 가장 상단을 가르킴
@@ -105,3 +89,183 @@ def validate_PW(pw):
 #         return 'True'
 #     else:
 #         return 'False'
+
+def account_Check(id, pw):
+    ref = db.reference('member')
+    AccountExist = ref.order_by_child('id').equal_to(id).get()
+
+    if AccountExist:
+        for key, value in AccountExist.items():
+            if value.get('password') == pw:
+                session['nickname'] = value.get('nickname')
+                return True
+    else:
+        return False
+    
+def get_api_total(search):
+    # 이전 쿼리에서 가져온 데이터 중에서 6번째 데이터의 'year' 값을 가져옴
+    #ref = db.reference('piece').order_by_child('year').limit_to_first(5).get()
+    #last_year = list(ref.values())[-1]['year']
+
+    # 'year' 필드가 'last_year' 이상인 데이터 중에서 다섯 번째까지의 데이터를 가져옴
+    #ref = db.reference('works').order_by_child('year').start_at(last_year).limit_to_first(5).get()
+    #ref = db.reference('piece').order_by_child('year').limit_to_first(5).get()
+    # 가져온 데이터를 출력함
+    print(search)
+    if(search == None):
+        ref = db.reference('piece')
+        keys = ref.order_by_key().get()
+        total = math.ceil(len(keys)/10)
+    else:
+        ref = db.reference('piece')
+        data = ref.order_by_child('year')
+        matching_pieces = []
+        for item in data.get().values():
+            if str(search).lower() in str(item['name']).lower():
+                matching_pieces.append(item)
+        total = math.ceil(len(matching_pieces)/10)
+
+    return str(total)
+
+# def get_api_piece(cur_page, search):
+#     ref = db.reference('piece')
+
+#     # year 필드를 기준으로 내림차순으로 정렬된 데이터 가져오기
+#     query = ref.order_by_child('year')
+
+#     data = []
+#     for item in query.get().values():
+#         data.append(item)
+
+#     data.reverse() 
+
+#     # 데이터 슬라이싱
+#     start = (cur_page-1)*10
+#     end = cur_page*10
+#     sliced_data = data[start:end]
+
+#     # 결과 출력
+#     for item in sliced_data:
+#         print(item)
+
+#     return "ok"
+
+def update_api_data():
+    return "json?"
+
+def create_api_data():
+    ref = db.reference('')
+    # piece_ref = ref.update(data_list)
+    # user_ref.set()
+    #user_ref = ref.push()
+    #user_ref.set({
+    #    'id' : str(id),
+    #    'password' : str(pw),
+    #    'nickname' : str(nickname),
+    #    'email' : str(email)
+    #    })
+
+    url1 = 'http://openapi.seoul.go.kr:8088/6e467277426a61633339737752416b/xml/tvGonggongArt/1/1000/'
+    url2 = 'http://openapi.seoul.go.kr:8088/6e467277426a61633339737752416b/xml/tvGonggongAllArt/1/1000/'
+    url3 = 'http://openapi.seoul.go.kr:8088/6e467277426a61633339737752416b/xml/gongGongArtEtc/1/1000/'
+    
+    response1 = requests.get(url1)
+    response2 = requests.get(url2)
+    response3 = requests.get(url3)
+    
+    xml_data1 = xmltodict.parse(response1.text)  
+    xml_data2 = xmltodict.parse(response2.text)
+    xml_data3 = xmltodict.parse(response3.text)      
+    
+    #data_list = []
+        # data = 
+        # data_list.append({item['GA_KNAME']: data})
+
+    for item in xml_data2['tvGonggongAllArt']['row']:
+        name = str(item['GA_KNAME'])
+        name = name.replace('/', '_').replace('(', '_').replace(')', '_').replace('.', '_').replace('$', '_').replace('#', '_').replace('[', '_').replace(']', '_')
+
+        ref.child('piece').child(name).set({
+            'name': item['GA_KNAME'],
+            'year': item['GA_INS_DATE'],
+            'address': item['GA_ADDR'],
+            'description': 'none',
+        })
+
+        # data = 
+        # data_list.append({item['GA_KNAME']: data})
+    for item in xml_data1['tvGonggongArt']['row']:
+        name = str(item['GA_KNAME'])
+        name = name.replace('/', '_').replace('(', '_').replace(')', '_').replace('.', '_').replace('$', '_').replace('#', '_').replace('[', '_').replace(']', '_')
+
+        if item['GA_DETAIL'] == '' or item['GA_DETAIL'] == ' ' or item['GA_DETAIL'] == None:
+            ref.child('piece').child(name).set({
+            'name': item['GA_KNAME'],
+            'year': item['GA_INS_DATE'],
+            'address': item['GA_ADDR1'],
+            'description': 'none',
+            })
+        else:
+            ref.child('piece').child(name).set({
+            'name': item['GA_KNAME'],
+            'year': item['GA_INS_DATE'],
+            'address': item['GA_ADDR1'],
+            'description': item['GA_DETAIL'],
+            })  
+
+    for item in xml_data3['gongGongArtEtc']['row']:
+        name = str(item['GA_KNAME'])
+        name = name.replace('/', '_').replace('(', '_').replace(')', '_').replace('.', '_').replace('$', '_').replace('#', '_').replace('[', '_').replace(']', '_')
+
+        if item['GA_DETAIL'] == '' or item['GA_DETAIL'] == ' ' or item['GA_DETAIL'] == None:
+            ref.child('piece').child(name).set({
+            'name': item['GA_KNAME'],
+            'year': item['GA_INS_DATE'],
+            'address': item['GA_ADDR1'],
+            'description': 'none',
+            })
+        else:
+            ref.child('piece').child(name).set({
+            'name': item['GA_KNAME'],
+            'year': item['GA_INS_DATE'],
+            'address': item['GA_ADDR1'],
+            'description': item['GA_DETAIL'],
+            })  
+          
+        # data_list.append({item['GA_KNAME']: data})
+
+    # 추출한 데이터를 JSON으로 변환
+    #json_data = json.dumps(data_list, ensure_ascii=False)
+
+    # soup = BeautifulSoup(response2.text, 'xml')
+
+    # rows = soup.find_all('row')
+    # data = []
+    # for row in rows:
+    #     item = {}
+    #     item['작품명'] = row.GA_KNAME.text
+    #     item['연도'] = row.GA_INS_DATE.text
+    #     item['주소'] = row.GA_ADDR.text
+    #     item['상세설명'] = 'none'
+    #     data.append(item)
+
+    #print(data_list)
+    
+    #json_data = json.dumps(data_list, ensure_ascii=False)
+
+    #with open('json/artlist.json', 'w', encoding='utf-8') as f:
+    #    f.write(json_data)
+
+    return "Update"
+
+
+def get_geo_value(address):
+    geolocoder = Nominatim(user_agent = 'South Korea', timeout=None)
+    geo = geolocoder.geocode(address)
+    crd = {"lat": str(geo.latitude), "lng": str(geo.longitude)}
+
+    return crd
+
+def print_geo_value():
+    crd = get_geo_value("서울특별시 마포구 상암동 478-1")
+    return str(crd['lat']) + " & " + str(crd['lng'])
